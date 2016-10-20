1f4d63f93dece9ad6dd3508425bd2ab6c287f639
Jesse Wallace <business@c0deo.us>
2016-10-19 22:21:37 -0500

    Init

---
+++ pytemp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# < name >
+#
+# < description >
+#
+# (c) 2016 < Author > (@<twitter>)
+# < author website >
+
+
+def main():
+    # Do things #
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nQuitting...')
+        exit()
+
+
+
+''' License '''
